7b72dc4808231550862f242093759bfa34e4abb8
PPMT <phyophyomyatthu3@gmail.com>
2025-02-12 23:59:33 +0900

    Feat : SQL Data Connection

--- PPMTDotNetTraining.ConsoleApp/Program.cs
+++ PPMTDotNetTraining.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
@@ -15,16 +17,50 @@ Console.WriteLine("Hello, World!");
 //Create SQL connection
 //Crtl +.
 
-string connectionString = "Data Source=DESKTOP-3HK6N3Q;Initial Catalog=PPMTDotNetTrainingBatch5;User ID=DESKTOP-3HK6N3Q￥yukin";
+
+string connectionString = "Data Source=DESKTOP-3HK6N3Q;Initial Catalog=PPMTDotNetTraining;User ID=sa;Password=sasa@123";
 Console.WriteLine(connectionString);
 SqlConnection connection = new SqlConnection(connectionString);
-//Reason is coz of max connection number
-//To prevent connection timeout error
 Console.WriteLine(value: "Connection opening...");
 connection.Open();
 Console.WriteLine(value: "Connection Opened...");
+
+string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where DeleteFlag=0";
+
+SqlCommand cmd = new SqlCommand(query,connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
+
+SqlDataReader reader = cmd.ExecuteReader();
+while (reader.Read())
+{
+    Console.WriteLine(reader["BlogTitle"]);
+    Console.WriteLine(reader["BlogAuthor"]);
+    Console.WriteLine(reader["BlogContent"]);
+    Console.WriteLine(reader["BlogId"]);
+}
+
+
+
+
 Console.WriteLine(value: "Connection Closing...");
 connection.Close();
 Console.WriteLine(value: "Connection Closed...");
 Console.ReadKey();
+
+
+//foreach (DataRow dr in dt.Rows)
+//{
+//    Console.WriteLine(dr["BlogTitle"]);
+//    Console.WriteLine(dr["BlogAuthor"]);
+//    Console.WriteLine(dr["BlogContent"]);
+//    Console.WriteLine(dr["BlogId"]);
+
+//}
 
