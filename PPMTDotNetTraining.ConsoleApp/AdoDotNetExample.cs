3f9c9b196fa1737cf375d56622879119d8a4c413
PPMT <phyophyomyatthu3@gmail.com>
2025-02-14 21:45:36 +0900

    Feat : Add Class : Read

---
+++ PPMTDotNetTraining.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPMTDotNetTraining.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+
+        private readonly string _connectionString= "Data Source=DESKTOP-3HK6N3Q;Initial Catalog=PPMTDotNetTraining;User ID=sa;Password=sasa@123";
+
+
+        public void Read()
+        {
+            
+            Console.WriteLine(_connectionString);
+            SqlConnection connection = new SqlConnection(_connectionString);
+            Console.WriteLine(value: "Connection opening...");
+            connection.Open();
+            Console.WriteLine(value: "Connection Opened...");
+
+            string query = @"SELECT [BlogId]
+          ,[BlogTitle]
+          ,[BlogAuthor]
+          ,[BlogContent]
+          ,[DeleteFlag]
+          FROM [dbo].[Tbl_Blog] where DeleteFlag=0";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //adapter.Fill(dt);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+                Console.WriteLine(reader["BlogId"]);
+            }
+
+
+
+
+            Console.WriteLine(value: "Connection Closing...");
+            connection.Close();
+            Console.WriteLine(value: "Connection Closed...");
+            Console.ReadKey();
+        }
+     }
+
+    
+}
--- PPMTDotNetTraining.ConsoleApp/Program.cs
+++ PPMTDotNetTraining.ConsoleApp/Program.cs
@@ -2,8 +2,9 @@
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using PPMTDotNetTraining.ConsoleApp;
 
-Console.WriteLine("Hello, World!");
+
 //C# and Database Connection
 //ADO.NET
 //Dapper(ORM)
@@ -17,42 +18,9 @@ Console.WriteLine("Hello, World!");
 //Create SQL connection
 //Crtl +.
 
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
 
-string connectionString = "Data Source=DESKTOP-3HK6N3Q;Initial Catalog=PPMTDotNetTraining;User ID=sa;Password=sasa@123";
-Console.WriteLine(connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
-Console.WriteLine(value: "Connection opening...");
-connection.Open();
-Console.WriteLine(value: "Connection Opened...");
-
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_Blog] where DeleteFlag=0";
-
-SqlCommand cmd = new SqlCommand(query,connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-//DataTable dt = new DataTable();
-//adapter.Fill(dt);
-
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-    Console.WriteLine(reader["BlogId"]);
-}
-
-
-
-
-Console.WriteLine(value: "Connection Closing...");
-connection.Close();
-Console.WriteLine(value: "Connection Closed...");
-Console.ReadKey();
 
 
 //foreach (DataRow dr in dt.Rows)
