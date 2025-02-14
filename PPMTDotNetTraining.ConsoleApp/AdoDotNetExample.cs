62aeaebd69be9644e87bfd832578a210c64cf07a
PPMT <phyophyomyatthu3@gmail.com>
2025-02-14 22:10:11 +0900

    Feat : Add Class : Create

--- PPMTDotNetTraining.ConsoleApp/AdoDotNetExample.cs
+++ PPMTDotNetTraining.ConsoleApp/AdoDotNetExample.cs
@@ -10,12 +10,12 @@ namespace PPMTDotNetTraining.ConsoleApp
     public class AdoDotNetExample
     {
 
-        private readonly string _connectionString= "Data Source=DESKTOP-3HK6N3Q;Initial Catalog=PPMTDotNetTraining;User ID=sa;Password=sasa@123";
+        private readonly string _connectionString = "Data Source=DESKTOP-3HK6N3Q;Initial Catalog=PPMTDotNetTraining;User ID=sa;Password=sasa@123";
 
 
         public void Read()
         {
-            
+
             Console.WriteLine(_connectionString);
             SqlConnection connection = new SqlConnection(_connectionString);
             Console.WriteLine(value: "Connection opening...");
@@ -43,15 +43,88 @@ namespace PPMTDotNetTraining.ConsoleApp
                 Console.WriteLine(reader["BlogId"]);
             }
 
-
-
-
             Console.WriteLine(value: "Connection Closing...");
             connection.Close();
             Console.WriteLine(value: "Connection Closed...");
             Console.ReadKey();
         }
-     }
+         
 
-    
+
+       public void Create()
+        {
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine(dr["BlogTitle"]);
+            //    Console.WriteLine(dr["BlogAuthor"]);
+            //    Console.WriteLine(dr["BlogContent"]);
+            //    Console.WriteLine(dr["BlogId"]);
+
+            //}
+
+            Console.Write("Blog Title:");
+            string title = Console.ReadLine();
+
+            Console.Write("Blog Author:");
+            string author = Console.ReadLine();
+
+            Console.Write("Blog Content:");
+            string content = Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            //BAD example
+            //SQL Injection can occur
+
+            //String Query2 = $@"
+            //INSERT INTO [dbo].[Tbl_Blog]
+            //           ([BlogTitle]
+            //           ,[BlogAuthor]
+            //           ,[BlogContent]
+            //           ,[DeleteFlag])
+            //     VALUES
+            //           ('{title}'
+            //           ,'{author}'
+            //           ,'{content}'
+            //           ,0)";
+
+            String Query = @"
+            INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+            VALUES
+           (@BlogTitle,
+           @BlogAuthor,
+           @BlogContent,
+              0)";
+
+
+            SqlCommand cmd = new SqlCommand(Query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            //if (result ==1)
+            //{
+            //    Console.WriteLine("Saving Successful");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Saving Failed");
+            //}
+
+            //Ternary Operator
+            Console.WriteLine(result == 1 ? "Saving Successful" : "Saving Failed");
+
+
+        }
+
+
+    }
+
 }
